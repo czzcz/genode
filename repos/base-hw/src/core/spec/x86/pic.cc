10dc83ade968407c02a07511f0f7fc83440a4c3f
Reto Buerki <reet@codelabs.ch>
2015-02-17 09:41:36 +0100

    hw: Initialize PICs
    Set master vector offset to 0x20, slave vector offset to 0x28 and enable
    8086 mode.

--- repos/base-hw/src/core/spec/x86/pic.cc
+++ repos/base-hw/src/core/spec/x86/pic.cc
@@ -1,3 +1,5 @@
+#include <port_io.h>
+
 #include "pic.h"
 
 using namespace Genode;
@@ -16,4 +18,34 @@ enum {
 };
 
 
-Pic::Pic() { }
+Pic::Pic()
+{
+	unsigned char a1, a2;
+
+	/* Save masks */
+	a1 = inb(PIC1_DATA);
+	a2 = inb(PIC2_DATA);
+
+	/* Start initialization sequence in cascade mode */
+	outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4);
+	outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
+
+	/* ICW2: Master PIC vector offset (32) */
+	outb(PIC1_DATA, 0x20);
+	/* ICW2: Slave PIC vector offset (40) */
+	outb(PIC2_DATA, 0x28);
+
+	/* ICW3: Tell Master PIC that there is a slave PIC at IRQ2 */
+	outb(PIC1_DATA, 4);
+
+	/* ICW3: Tell Slave PIC its cascade identity */
+	outb(PIC2_DATA, 2);
+
+	/* ICW4: Enable 8086 mode */
+	outb(PIC1_DATA, ICW4_8086);
+	outb(PIC2_DATA, ICW4_8086);
+
+	/* Restore saved masks */
+	outb(PIC1_DATA, a1);
+	outb(PIC2_DATA, a2);
+}
