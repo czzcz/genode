09578b77924127fab7216f0fcccdd824d943e9fa
Reto Buerki <reet@codelabs.ch>
2015-02-17 11:03:13 +0100

    hw: Declare PIC vector offset constants

--- repos/base-hw/src/core/spec/x86/pic.cc
+++ repos/base-hw/src/core/spec/x86/pic.cc
@@ -15,6 +15,9 @@ enum {
 	ICW1_INIT    = 0x10,      /* Initialization                 */
 	ICW1_ICW4    = 0x01,      /* Enable ICW4                    */
 	ICW4_8086    = 0x01,      /* 8086/88 (MCS-80/85) mode       */
+
+	MASTER_VEC_OFFSET = 0x20,
+	SLAVE_VEC_OFFSET  = 0x28,
 };
 
 
@@ -31,9 +34,9 @@ Pic::Pic()
 	outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
 
 	/* ICW2: Master PIC vector offset (32) */
-	outb(PIC1_DATA, 0x20);
+	outb(PIC1_DATA, MASTER_VEC_OFFSET);
 	/* ICW2: Slave PIC vector offset (40) */
-	outb(PIC2_DATA, 0x28);
+	outb(PIC2_DATA, SLAVE_VEC_OFFSET);
 
 	/* ICW3: Tell Master PIC that there is a slave PIC at IRQ2 */
 	outb(PIC1_DATA, 4);
